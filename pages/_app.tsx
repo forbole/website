f4180cddca712a770f79c92cb9d3c49a99c3ec2c
Wingman J. Lee <42913823+ryuash@users.noreply.github.com>
2020-07-13 19:05:43 +0800

    Feature/setup i18n (#2)
    * wip: adding i18n
    
    * add: i18n
    
    * refactor: index page
    
    * update: use yarn
    
    Co-authored-by: Rachel Ho <rachelhoyuenyi@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 import "../scss/style.scss";
 import { AppProps } from "next/app";
+import { appWithTranslation } from "../i18n";
 
 // This default export is required in a new `pages/_app.js` file.
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
+export default appWithTranslation(MyApp);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,34 @@
 import Link from "next/link";
 import Layout from "@components/Layout";
 import Head from "next/head";
+import { useTranslation } from "../i18n";
 
-const IndexPage = () => (
-  <Layout title="Forbole">
-    <div>
-      <Head>
-        <title>Forbole</title>
-      </Head>
-      <div className="hero">
-        <h1>Co-Building Interchain</h1>
-        <p>
-          <Link href="/about">
-            <a>About</a>
-          </Link>
-        </p>
-      </div>
+const IndexPage = () => {
+  const { t } = useTranslation("home");
+  return (
+    <Layout title="Forbole">
+      <div>
+        <Head>
+          <title>Forbole</title>
+        </Head>
+        <div className="hero">
+          <h1>Co-Building Interchain</h1>
+          <p>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </p>
+        </div>
 
-      <div className="container">
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis, in.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero sed qui
-          nulla quisquam possimus voluptas optio repellat molestias corporis,
-          non in perspiciatis sint voluptatem velit ratione quis labore autem
-          temporibus.
-        </p>
+        <div className="container">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis, in.
+          </p>
+          <p>{t("placeholder")}</p>
+        </div>
       </div>
-    </div>
-  </Layout>
-);
+    </Layout>
+  );
+};
 
 export default IndexPage;
