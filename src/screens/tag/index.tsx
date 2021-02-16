ec4a9fbf9e334f39012cfb1584766498541ed019
Rachel Ho <rachelhoyuenyi@gmail.com>
2021-02-16 15:57:44 +0800

    tag page rendering (#207)
    * tag page redenering
    
    * tag page rendering

--- src/screens/tag/index.tsx
+++ src/screens/tag/index.tsx
@@ -25,38 +25,51 @@ const TagTitlePosts = (props: any) => {
   }, [props]);
   const { t } = useTranslation("blog");
   useBlogHook(error, t);
-  return (
-    <Layout
-      title={isLoading ? t("forbole") : props.post?.title}
-      navColor={colors.gray600}
-      mobileNavColor={colors.gray600}
-      description={isLoading ? t("excerpt") : props.post?.excerpt}
-      type="article"
-      image={isLoading ? t("forbole") : props.post?.featureImage}
-      keywords={tags.map((x) => x.name ?? "")}
-    >
-      <BlogCSS>
-        <MaxWidthContainerCSS>
-          {isLoading ? (
+  if (isLoading) {
+    return (
+      <Layout
+        title={t("forbole")}
+        navColor={colors.gray600}
+        mobileNavColor={colors.gray600}
+        description={t("excerpt")}
+        type="article"
+        image={t("forbole")}
+      >
+        <BlogCSS>
+          <MaxWidthContainerCSS>
             <TagDetailsLoader />
-          ) : (
-            <>
-              <TagPosts
-                main={props.post[0]}
-                blogs={props.post.slice(1)}
-                meta={meta}
-              />
-              <SideCSS>
-                <TitlePosts posts={sidePosts} />
-                <Tags tags={tags} />
-                <Twitter />
-              </SideCSS>
-            </>
-          )}
-        </MaxWidthContainerCSS>
-      </BlogCSS>
-    </Layout>
-  );
+          </MaxWidthContainerCSS>
+        </BlogCSS>
+      </Layout>
+    );
+  } else {
+    return (
+      <Layout
+        title={props.post.title}
+        navColor={colors.gray600}
+        mobileNavColor={colors.gray600}
+        description={props.post.excerpt}
+        type="article"
+        image={props.post.featureImage}
+        keywords={tags.map((x) => x.name ?? "")}
+      >
+        <BlogCSS>
+          <MaxWidthContainerCSS>
+            <TagPosts
+              main={props.post[0]}
+              blogs={props.post.slice(1)}
+              meta={meta}
+            />
+            <SideCSS>
+              <TitlePosts posts={sidePosts} />
+              <Tags tags={tags} />
+              <Twitter />
+            </SideCSS>
+          </MaxWidthContainerCSS>
+        </BlogCSS>
+      </Layout>
+    );
+  }
 };
 
 export default TagTitlePosts;
