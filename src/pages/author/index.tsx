03a31b4e36c4042f2acdaa898b30cd746c223bcc
Rachel Ho <rachelhoyuenyi@gmail.com>
2021-06-07 17:17:03 +0800

    added index for src/pages/author (#307)
    Co-authored-by: Kwun Yeung <kwun@forbole.com>

---
+++ src/pages/author/index.tsx
@@ -0,0 +1,47 @@
+import Blog from "@screens/blog";
+import { getPosts, getTags } from "@api/posts";
+import { Post, Tag } from "@models";
+import { removeInternalTags } from "@utils/remove_internal_tags";
+function BlogPage(props: any) {
+  return <Blog {...props} />;
+}
+
+BlogPage.getInitialProps = async ({ query }) => {
+  let formattedPosts = [];
+  let formattedSidePosts = [];
+  let formattedTags = [];
+  let meta = {};
+  let error = false;
+  try {
+    const fetchQuery: any = {};
+    if (query.page) {
+      fetchQuery.page = query.page;
+    }
+
+    const [tags, posts, sidePosts] = await Promise.all([
+      getTags(),
+      getPosts(fetchQuery),
+      getPosts({
+        limit: 10,
+      }),
+    ]);
+
+    formattedPosts = posts.map((post) => Post.fromJson(post, {}));
+    formattedSidePosts = sidePosts.map((post) => Post.fromJson(post, {}));
+    formattedTags = removeInternalTags(tags).map((tag) => Tag.fromJson(tag));
+    meta = posts?.meta;
+  } catch (err) {
+    error = true;
+    console.log(error, "error");
+  }
+
+  return {
+    posts: formattedPosts,
+    tags: formattedTags,
+    sidePosts: formattedSidePosts,
+    meta,
+    error,
+  };
+};
+
+export default BlogPage;
