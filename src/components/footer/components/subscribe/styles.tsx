c8087dbc58ce973128648fcc1cee0ce94dc582c1
Rachel Ho <rachelhoyuenyi@gmail.com>
2021-06-09 14:52:56 +0800

    subscribe button (#319)

--- src/components/footer/components/subscribe/styles.tsx
+++ src/components/footer/components/subscribe/styles.tsx
@@ -6,26 +6,6 @@ const { colors } = theme;
 export const SubscribeCSS = styled.div`
   color: ${colors.white};
   padding-bottom: 1rem;
-  input::-webkit-input-placeholder {
-    color: white !important;
-  }
-  input:-moz-placeholder {
-    color: white !important;
-  }
-  input::-moz-placeholder {
-    color: white !important;
-  }
-  input:-ms-input-placeholder {
-    color: white !important;
-  }
-
-  .ui.input > input {
-    color: rgba(255, 255, 255, 0.8);
-    background: rgba(255, 255, 255, 0.3);
-    font-size: 0.8rem;
-    font-weight: 300;
-    border-radius: 0.25rem 0rem 0rem 0.25rem;
-  }
 
   h1 {
     font-size: 1rem;
@@ -38,25 +18,15 @@ export const SubscribeCSS = styled.div`
     font-weight: 200;
   }
 
-  .ui.action.input {
-    width: 100%;
-  }
-
-  .ui.input {
-    position: relative;
-    right: 0;
-  }
   .ui.button {
     font-weight: 300;
     color: rgba(255, 255, 255, 1);
     background: ${colors.forboleRed};
-    border-radius: 0rem 0.25rem 0.25rem 0rem;
+    // border-radius: 0rem 0.25rem 0.25rem 0rem;
+    width: 100%;
   }
   ${media.bigDesktop`
   padding-bottom: 0;
   width: 100%;
-  .ui.action.input {
-    width: 100%;
-  }
   `}
 `;
