4755cf63def886712b2fb456306da45679081a80
rachelhox <rachelhoyuenyi@gmail.com>
2022-06-13 11:02:49 +0800

    ghost cms set-up

---
+++ src/components/api/authors/index.ts
@@ -0,0 +1,36 @@
+/* eslint-disable no-console */
+import { ghostApi as api } from '../index';
+// import { IPost } from "./interface";
+
+/** Get blog posts by tag */
+export const getAuthorBySlug = async (query: { author: string }) => {
+  try {
+    const authorInfo = await api.authors.read({
+      slug: `${query.author}`,
+    });
+    return authorInfo ?? null;
+  } catch (err) {
+    console.error(`tag: ${err}`);
+    return [];
+  }
+};
+
+/** Get blog posts by author */
+export const getPostsByAuthor = async (query: {
+  author: string;
+  page?: number;
+}) => {
+  try {
+    const posts = await api.posts.browse({
+      filter: `author:${query.author}`,
+      include: 'tags,authors',
+      limit: 5,
+      page: query?.page || 1,
+      formats: 'html',
+    });
+    return posts ?? null;
+  } catch (err) {
+    console.error(`tag: ${err}`);
+    return [];
+  }
+};
