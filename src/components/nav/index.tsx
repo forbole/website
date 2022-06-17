785756746ef8b0a0c2b48014ad02c42d6b8d832d
rachelhox <rachelhoyuenyi@gmail.com>
2022-06-17 13:44:46 +0800

    nav: background blur on scroll

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { Box, useTheme } from '@mui/material';
 import { Forbole as ForboleLogo } from '@icons';
 import { NavMenu } from './components';
+import { useNavHook } from './hooks';
 
 interface NavProps {
   navLink: string;
@@ -11,86 +12,92 @@ interface NavProps {
 
 const Nav = ({ navLink }: NavProps) => {
   const theme = useTheme();
+  const { displayBackground } = useNavHook();
   return (
-    // <Box
-    //   sx={{
-    //     display: 'flex',
-    //     justifyContent: 'center',
-    //     width: '100%',
-    //     background: 'transparent',
-    //     backdropFilter: 'blur(16px)',
-    //     webkitBackdropFilter: 'blur(16px)',
-    //     position: 'relative',
-    //   }}
-    // >
     <Box
       sx={{
-        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
         position: 'fixed',
         top: 0,
-        overflowY: 'scroll',
-        overflowX: 'hidden',
         zIndex: 2,
-        [theme.breakpoints.up('laptop')]: {
-          maxWidth: '1200px',
-          width: '100%',
-          position: 'fixed',
-          top: 30,
-          margin: 'auto',
-          left: '50vw',
-          transform: 'translate(-50%, -50%)',
-          zIndex: 2,
-        },
+        width: '100%',
+        background: displayBackground
+          ? 'rgba(114, 28, 78, 0.1)'
+          : 'transparent',
+        backdropFilter: displayBackground ? 'blur(16px)' : 'none',
+        webkitBackdropFilter: 'blur(16px)',
+        height: '100px',
       }}
     >
       <Box
         sx={{
-          background: 'transparent',
-          paddingTop: theme.spacing(4),
-          display: 'flex',
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'center',
+          width: '100%',
+          position: 'fixed',
+          top: 0,
+          overflowY: 'scroll',
+          overflowX: 'hidden',
+          zIndex: 2,
           [theme.breakpoints.up('laptop')]: {
-            padding: theme.spacing(4, 0, 0, 0),
-            justifyContent: 'space-between',
+            maxWidth: '1200px',
+            width: '100%',
+            position: 'fixed',
+            top: 30,
+            margin: 'auto',
+            left: '50vw',
+            transform: 'translate(-50%, -50%)',
+            zIndex: 2,
           },
         }}
       >
         <Box
           sx={{
-            // margin: 'auto',
+            background: 'transparent',
+            paddingTop: theme.spacing(4),
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
             [theme.breakpoints.up('laptop')]: {
-              margin: 0,
+              padding: theme.spacing(4, 0, 0, 0),
+              justifyContent: 'space-between',
             },
           }}
         >
-          <Link href="/">
-            <a>
-              <ForboleLogo
-                color={
-                  theme.palette.mode === 'dark'
-                    ? theme.palette.primary.main
-                    : theme.palette.custom.forbole.red
-                }
-              />
-            </a>
-          </Link>
-        </Box>
-        <Box
-          sx={{
-            [theme.breakpoints.down('laptop')]: {
-              position: 'absolute',
-              top: theme.spacing(4),
-              right: theme.spacing(4),
-            },
-          }}
-        >
-          <NavMenu link={navLink} />
+          <Box
+            sx={{
+              // margin: 'auto',
+              [theme.breakpoints.up('laptop')]: {
+                margin: 0,
+              },
+            }}
+          >
+            <Link href="/">
+              <a>
+                <ForboleLogo
+                  color={
+                    theme.palette.mode === 'dark'
+                      ? theme.palette.primary.main
+                      : theme.palette.custom.forbole.red
+                  }
+                />
+              </a>
+            </Link>
+          </Box>
+          <Box
+            sx={{
+              [theme.breakpoints.down('laptop')]: {
+                position: 'absolute',
+                top: theme.spacing(4),
+                right: theme.spacing(4),
+              },
+            }}
+          >
+            <NavMenu link={navLink} />
+          </Box>
         </Box>
       </Box>
     </Box>
-    // </Box>
   );
 };
 
