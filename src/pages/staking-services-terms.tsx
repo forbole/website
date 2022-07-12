6186222153e9dfd27c54348c8a20896235402e09
rachelhox <rachelhoyuenyi@gmail.com>
2022-07-12 16:49:50 +0800

    staking services terms

---
+++ src/pages/staking-services-terms.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import StakingServicesTerms from '@screens/staking_services_terms';
+
+const StakingServicesTermsPage: NextPage = () => {
+  return <StakingServicesTerms />;
+};
+
+export default StakingServicesTermsPage;
