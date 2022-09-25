aaa1bd3ac6b248279d68c09fbb92206c501fc808
rachelhox <rachelhoyuenyi@gmail.com>
2022-09-25 20:12:50 +0800

    mobile autocomplete styles

--- src/screens/staking/components/networks/components/search_bar/useStyles.ts
+++ src/screens/staking/components/networks/components/search_bar/useStyles.ts
@@ -19,8 +19,9 @@ const useStyles = () => {
           gap: theme.spacing(2),
           alignItems: 'center',
           padding: theme.spacing(0, 2, 0, 4),
-          backgroundColor: 'rgba(29, 30, 34, 0.32)',
           backdropFilter: 'blur(16px)',
+          background:
+            'linear-gradient(180deg, #FFFFFF 0%, rgba(255, 255, 255, 0) 100%)',
           '& .searchbox__cancel-btn': {
             display: 'inline-flex',
           },
@@ -39,6 +40,7 @@ const useStyles = () => {
           width: theme.spacing(36.375),
           minWidth: theme.spacing(36.375),
           height: theme.spacing(4.75),
+          float: 'right',
         },
         '& .MuiInputBase-root': {
           padding: `${theme.spacing(0.5, 2.25, 0.5, 2.25)} !important`,
@@ -77,8 +79,11 @@ const useStyles = () => {
           height: `${theme.spacing(3)} !important`,
           borderRadius: '100%',
           padding: 0,
-          border: '0 none',
           margin: 0,
+          border: '1px solid rgba(177, 177, 242, 0.3)',
+          /* Shadow/Secondary (Validator)/Drop Shadow 01 */
+          boxShadow:
+            '0px 8px 22px -6px rgba(2, 38, 225, 0.12), 0px 14px 64px -4px rgba(2, 38, 225, 0.12)',
         },
       },
       '& .MuiListItemText-root': {
@@ -86,9 +91,9 @@ const useStyles = () => {
         flex: `1 0 ${theme.spacing(3)} !important`,
         padding: '0 !important',
         /* Neutral Color/Light Grey 01 */
-        color: theme.palette.grey[300],
+        color: theme.palette.custom.forbole.indigo6,
         fontStyle: 'normal',
-        fontWeight: 400,
+        fontWeight: 600,
         fontSize: theme.typography.body2.fontSize,
         lineHeight: 1.42,
       },
@@ -116,9 +121,13 @@ const useStyles = () => {
         right: '0 !important',
         padding: `${theme.spacing(0, 4)} !important`,
         width: '100% !important',
-        backgroundColor: 'rgba(29, 30, 34, 0.32) !important',
+        backgroundColor:
+          'linear-gradient(180deg, #FFFFFF 0%, rgba(255, 255, 255, 0) 100%) !important',
         backdropFilter: 'blur(16px)',
         transform: 'none !important',
+        /* Shadow/Secondary (Validator)/Drop Shadow 02 */
+        boxShadow:
+          '0px 6px 14px -6px rgba(2, 38, 225, 0.12), 0px 10px 32px -4px rgba(2, 38, 225, 0.1)',
       },
     },
   };
