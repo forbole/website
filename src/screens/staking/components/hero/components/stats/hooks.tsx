f13294ef46e7a863a685283bbc13134463666a1d
rachelhox <rachelhoyuenyi@gmail.com>
2022-12-01 18:43:08 +0800

    total TVL: elrond added

--- src/screens/staking/components/hero/components/stats/hooks.tsx
+++ src/screens/staking/components/hero/components/stats/hooks.tsx
@@ -2,11 +2,20 @@
 /* eslint-disable no-console */
 import { useState, useMemo } from 'react';
 import { useQuery, gql } from '@apollo/client';
-import { getCosmosUsersCount, getAllCosmosTVL } from '@graphql/queries';
+import {
+  getCosmosUsersCount,
+  getAllCosmosTVL,
+  getElrondTVL,
+  // getSolanaTVL,
+  // getOasisTVL,
+} from '@graphql/queries';
+import { networkFunctions } from '@utils/network_functions';
 import { statsItems } from './config';
 
 export const useStatsHook = () => {
   const [stats, setStats] = useState(statsItems);
+  const [cosmosTVL, setCosmosTVL] = useState(0);
+  const [elrondTotalTVL, setElrondTotalTVL] = useState(0);
   const { loading: totalTVLLoading, data: totalTVL } = useQuery(
     gql`
       ${getAllCosmosTVL()}
@@ -19,9 +28,16 @@ export const useStatsHook = () => {
       `
     );
 
+  const { loading: elrondTVLLoading, data: elrondTVLData } = useQuery(
+    gql`
+      ${getElrondTVL()}
+    `
+  );
+
   useMemo(() => {
     if (!totalTVLLoading) {
       const { allCosmosTVL } = totalTVL;
+      setCosmosTVL(allCosmosTVL[0].cosmosTVL);
       statsItems.map((item, i) =>
         item.title === 'full tvl'
           ? setStats((prevStats) => ({
@@ -36,6 +52,35 @@ export const useStatsHook = () => {
     }
     return stats;
   }, [totalTVL, totalTVLLoading]);
+
+  useMemo(() => {
+    if (!elrondTVLLoading) {
+      const { elrondTVL } = elrondTVLData;
+      const elrondNetworkFunctions = networkFunctions.elrond;
+      const TVL = elrondNetworkFunctions.converter(elrondTVL[0].TVL);
+      setElrondTotalTVL(TVL);
+      const updatedTVL = Number(cosmosTVL) + Number(elrondTotalTVL);
+      statsItems.map((item, i) =>
+        item.title === 'full tvl'
+          ? setStats((prevStats) => ({
+              ...prevStats,
+              [i]: {
+                ...item,
+                stats: updatedTVL,
+              },
+            }))
+          : null
+      );
+    }
+    return stats;
+  }, [
+    elrondTVLData,
+    elrondTVLLoading,
+    cosmosTVL,
+    setCosmosTVL,
+    totalTVL,
+    totalTVLLoading,
+  ]);
 
   useMemo(() => {
     if (!cosmosUsersCountLoading) {
