3ff4250f9b6bab200072e34d1293686e9a8b92b1
rachelhox <rachelhoyuenyi@gmail.com>
2022-12-01 19:43:15 +0800

    FBW-85

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -18,8 +18,8 @@ const Home = () => {
       navLink="/"
       title={t('forbole')}
       description={t('description')}
-      image="/images/assets/website-preview.png"
-      twitterImage="/images/assets/website-preview.png"
+      image="/images/assets/image_forbole_website_preview.png"
+      twitterImage="/images/assets/image_forbole_website_preview.png"
       footer
       homeAnimation
     >
