40d494bef8771bfcdc4b60dfbe21d9c15efaf915
rachelhox <rachelhoyuenyi@gmail.com>
2022-12-02 18:24:21 +0800

    hooks: total user count

--- src/screens/staking/components/hero/components/stats/hooks.tsx
+++ src/screens/staking/components/hero/components/stats/hooks.tsx
@@ -3,11 +3,14 @@
 import { useState, useMemo, useEffect } from 'react';
 import { useQuery, gql } from '@apollo/client';
 import {
-  getCosmosUsersCount,
   getAllCosmosTVL,
   getElrondTVL,
   getSolanaTVL,
   getOasisTVL,
+  getCosmosUsersCount,
+  getSolanaUsersCount,
+  getElrondUsersCount,
+  getOasisUsersCount,
 } from '@graphql/queries';
 import { networkFunctions } from '@utils/network_functions';
 import { statsItems } from './config';
@@ -19,6 +22,11 @@ export const useStatsHook = () => {
   const [solanaTotalTVL, setSolanaTotalTVL] = useState(0);
   const [oasisTotalTVL, setOasisTotalTVL] = useState(0);
   const [totalTVL, setTotalTVL] = useState(0);
+  const [cosmosUser, setCosmosUser] = useState(0);
+  const [solanaUser, setSolanaUser] = useState(0);
+  const [elrondUser, setElrondUser] = useState(0);
+  const [oasisUser, setOasisUser] = useState(0);
+  const [totalUsers, setTotalUsers] = useState(0);
 
   const { loading: cosmosTotalTVLLoading, data: cosmosTotalTVL } = useQuery(
     gql`
@@ -51,6 +59,90 @@ export const useStatsHook = () => {
     `
   );
 
+  const { loading: solanaUserLoading, data: solanaUserData } = useQuery(
+    gql`
+      ${getSolanaUsersCount()}
+    `
+  );
+
+  const { loading: elrondUserLoading, data: elrondUserData } = useQuery(
+    gql`
+      ${getElrondUsersCount()}
+    `
+  );
+
+  const { loading: oasisUserLoading, data: oasisUserData } = useQuery(
+    gql`
+      ${getOasisUsersCount()}
+    `
+  );
+
+  useEffect(() => {
+    if (!cosmosUsersCountLoading) {
+      const { cosmosUsersCount } = cosmosUsersCountData;
+      setCosmosUser(cosmosUsersCount[0].usersCount);
+    }
+    if (!solanaUserLoading) {
+      const { solanaUsers } = solanaUserData;
+      setSolanaUser(solanaUsers.usersCount);
+    }
+    if (!elrondUserLoading) {
+      const { elrondUsers } = elrondUserData;
+      setElrondUser(elrondUsers[0].usersCount);
+    }
+    // if (!oasisUserLoading) {
+    //   const { oasisUsers } = oasisUserData;
+    //   setOasisUser(oasisUsers[0].usersCount);
+    // }
+  }, [
+    cosmosUsersCountLoading,
+    cosmosUsersCountData,
+    solanaUserLoading,
+    solanaUserData,
+    elrondUserLoading,
+    elrondUserData,
+    oasisUserLoading,
+    oasisUserData,
+  ]);
+
+  useMemo(() => {
+    let userCount = 0;
+    if (cosmosUser && Number(cosmosUser) !== 0) {
+      userCount += Number(cosmosUser);
+      setTotalUsers(userCount);
+    }
+    if (solanaUser && Number(solanaUser) !== 0) {
+      userCount += Number(solanaUser);
+      setTotalUsers(userCount);
+    }
+    if (elrondUser && Number(elrondUser) !== 0) {
+      userCount += Number(elrondUser);
+      setTotalUsers(userCount);
+    }
+    if (oasisUser && Number(oasisUser) !== 0) {
+      userCount += Number(oasisUser);
+      setTotalUsers(userCount);
+    }
+    statsItems.map((item, i) =>
+      item.title === 'users staking'
+        ? setStats((prevStats) => ({
+            ...prevStats,
+            [i]: {
+              ...item,
+              stats: totalUsers,
+            },
+          }))
+        : null
+    );
+  }, [
+    cosmosUser,
+    solanaUser,
+    elrondUser,
+    oasisUser,
+    totalUsers,
+    setTotalUsers,
+  ]);
+
   useEffect(() => {
     if (!cosmosTotalTVLLoading) {
       const { allCosmosTVL } = cosmosTotalTVL;
@@ -66,10 +158,10 @@ export const useStatsHook = () => {
       const { solanaTVL } = solanaTVLData;
       setSolanaTotalTVL(solanaTVL.TVL);
     }
-    if (!oasisTVLLoading) {
-      const { oasisTVL } = oasisTVLData;
-      setOasisTotalTVL(oasisTVL[0].TVL);
-    }
+    // if (!oasisTVLLoading) {
+    //   const { oasisTVL } = oasisTVLData;
+    //   setOasisTotalTVL(oasisTVL[0].TVL);
+    // }
   }, [
     cosmosTotalTVL,
     cosmosTotalTVLLoading,
@@ -83,19 +175,19 @@ export const useStatsHook = () => {
 
   useMemo(() => {
     let tvl = 0;
-    if (cosmosTVL !== 0) {
+    if (cosmosTVL && Number(cosmosTVL) !== 0) {
       tvl += Number(cosmosTVL);
       setTotalTVL(tvl);
     }
-    if (elrondTotalTVL !== 0) {
+    if (elrondTotalTVL && Number(elrondTotalTVL) !== 0) {
       tvl += Number(elrondTotalTVL);
       setTotalTVL(tvl);
     }
-    if (solanaTotalTVL !== 0) {
+    if (solanaTotalTVL && Number(solanaTotalTVL) !== 0) {
       tvl += Number(solanaTotalTVL);
       setTotalTVL(tvl);
     }
-    if (oasisTotalTVL !== 0) {
+    if (oasisTotalTVL && Number(oasisTotalTVL) !== 0) {
       tvl += Number(oasisTotalTVL);
       setTotalTVL(tvl);
     }
@@ -111,25 +203,32 @@ export const useStatsHook = () => {
         : null
     );
     return stats;
-  }, [cosmosTVL, elrondTotalTVL, solanaTotalTVL, oasisTotalTVL]);
+  }, [
+    cosmosTVL,
+    elrondTotalTVL,
+    solanaTotalTVL,
+    oasisTotalTVL,
+    totalTVL,
+    setTotalTVL,
+  ]);
 
-  useMemo(() => {
-    if (!cosmosUsersCountLoading) {
-      const { cosmosUsersCount } = cosmosUsersCountData;
-      statsItems.map((item, i) =>
-        item.title === 'users staking'
-          ? setStats((prevStats) => ({
-              ...prevStats,
-              [i]: {
-                ...item,
-                stats: cosmosUsersCount[0].usersCount,
-              },
-            }))
-          : null
-      );
-    }
-    return stats;
-  }, [cosmosUsersCountData, cosmosUsersCountLoading]);
+  // useMemo(() => {
+  //   if (!cosmosUsersCountLoading) {
+  //     const { cosmosUsersCount } = cosmosUsersCountData;
+  //     statsItems.map((item, i) =>
+  //       item.title === 'users staking'
+  //         ? setStats((prevStats) => ({
+  //             ...prevStats,
+  //             [i]: {
+  //               ...item,
+  //               stats: cosmosUsersCount[0].usersCount,
+  //             },
+  //           }))
+  //         : null
+  //     );
+  //   }
+  //   return stats;
+  // }, [cosmosUsersCountData, cosmosUsersCountLoading]);
 
   return stats;
 };
