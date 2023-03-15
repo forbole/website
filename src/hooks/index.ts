433765985dc9b7c3ea685585b67557734b7bbc06
rachelhox <rachelhoyuenyi@gmail.com>
2023-03-15 14:06:28 +0800

    hk timezone hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,5 @@
 import { useWindowDimensions } from './get_screen_size';
 import { useGetComponentDimension } from './get_component_dimensions';
+import { useHKT } from './get_hk_timezone';
 
-export { useWindowDimensions, useGetComponentDimension };
+export { useWindowDimensions, useGetComponentDimension, useHKT };
