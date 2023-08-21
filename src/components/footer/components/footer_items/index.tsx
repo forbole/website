c58996a3f4ec5b221499398f12dc034acc413ff1
rachelhox <rachelhoyuenyi@gmail.com>
2023-08-21 16:12:39 +0800

    blog config

--- src/components/footer/components/footer_items/index.tsx
+++ src/components/footer/components/footer_items/index.tsx
@@ -1,155 +1,142 @@
-import React from "react";
-import { Box, Divider, Typography, useTheme } from "@mui/material";
-import useTranslation from "next-translate/useTranslation";
-import Link from "next/link";
-import { FooterProps } from "../../types";
-import useStyles from "./useStyles";
+import React from 'react';
+import { Box, Divider, Typography, useTheme } from '@mui/material';
+import useTranslation from 'next-translate/useTranslation';
+import Link from 'next/link';
+import { FooterProps } from '../../types';
+import useStyles from './useStyles';
 
 const FooterItems = ({ staking }: FooterProps) => {
   const theme = useTheme();
-  const { t } = useTranslation("common");
+  const { t } = useTranslation('common');
   const today = new Date();
   const year = today.getFullYear();
-  const styles=useStyles()
+  const styles = useStyles();
   return (
     <Box
       sx={{
-        width: "100%",
-        display: "flex",
-        justifyContent: "space-between",
-        [theme.breakpoints.down("laptop")]: {
-          flexDirection: "column-reverse",
-          gap: "32px",
-          fontSize: "20px",
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'space-between',
+        [theme.breakpoints.down('laptop')]: {
+          flexDirection: 'column-reverse',
+          gap: '32px',
+          fontSize: '20px',
         },
-        [theme.breakpoints.up("laptop")]: {
-          flexDirection: "row",
-          fontSize: "14px",
+        [theme.breakpoints.up('laptop')]: {
+          flexDirection: 'row',
+          fontSize: '14px',
         },
       }}
     >
       <Box
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          [theme.breakpoints.down("laptop")]: {
-            pt: "12px",
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          [theme.breakpoints.down('laptop')]: {
+            pt: '12px',
             // color: theme.palette.custom.forbole.indigo,
-            gap: "8px",
-            borderTop: "1px solid " + theme.palette.custom.forbole.grey02,
+            gap: '8px',
+            borderTop: '1px solid ' + theme.palette.custom.forbole.grey02,
           },
-          [theme.breakpoints.up("laptop")]: {
+          [theme.breakpoints.up('laptop')]: {
             // color: theme.palette.primary.main,
-            flexDirection: "row",
-            "& hr": {
+            flexDirection: 'row',
+            '& hr': {
               mx: 2,
             },
           },
         }}
       >
-        <Typography sx={{ 
+        <Typography
+          sx={{
             fontSize: '14px',
             fontStyle: 'normal',
             fontWeight: '590',
             lineHeight: '20px',
-            letterSpacing: '0.336px'}} >{t("copyright", { year })}</Typography>
+            letterSpacing: '0.336px',
+          }}
+        >
+          {t('copyright', { year })}
+        </Typography>
         <Divider
           orientation="vertical"
           variant="middle"
           flexItem
           sx={{
-            my:'4px',
+            my: '4px',
             borderColor: staking
               ? theme.palette.custom.forbole.indigo
               : theme.palette.primary.main,
-            [theme.breakpoints.down("laptop")]: {
-              display: "none",
+            [theme.breakpoints.down('laptop')]: {
+              display: 'none',
             },
           }}
         />
         <Link href="/terms-and-conditions">
-          <Typography
-           css={styles.word}
-          >
-            {t("tnc")}
-          </Typography>
+          <Typography css={styles.word}>{t('tnc')}</Typography>
         </Link>
         <Divider
           orientation="vertical"
           variant="middle"
           flexItem
-          sx={{ my:'4px',
+          sx={{
+            my: '4px',
             borderColor: staking
               ? theme.palette.custom.forbole.indigo
               : theme.palette.primary.main,
-            [theme.breakpoints.down("laptop")]: {
-              display: "none",
+            [theme.breakpoints.down('laptop')]: {
+              display: 'none',
             },
           }}
         />
         <Link href="/privacy-policy">
-          <Typography
-            css={styles.word}
-          >
-            {t("policy")}
-          </Typography>
+          <Typography css={styles.word}>{t('policy')}</Typography>
         </Link>
       </Box>
 
       <Box>
         <Box
           sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            [theme.breakpoints.down("laptop")]: {
-              gap: "12px",
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            [theme.breakpoints.down('laptop')]: {
+              gap: '12px',
               // color: theme.palette.custom.forbole.indigo,
             },
-            [theme.breakpoints.up("laptop")]: {
+            [theme.breakpoints.up('laptop')]: {
               // color: theme.palette.primary.main,
-              flexDirection: "row",
-              alignItems: "center",
+              flexDirection: 'row',
+              alignItems: 'center',
               paddingTop: 0,
               paddingBottom: 0,
-              "& hr": {
+              '& hr': {
                 mx: 2,
               },
             },
           }}
         >
           <Link href="https://drive.google.com/drive/folders/1w93woI10nRmH3ei6rfFQm4eZxyvk_4-2">
-            <Typography
-              css={styles.word}
-            >
-              {t("brand guide")}
-            </Typography>
+            <Typography css={styles.word}>{t('brand guide')}</Typography>
           </Link>
-          <Divider 
+          <Divider
             orientation="vertical"
             variant="middle"
             flexItem
             sx={{
-              my:'4px',
+              my: '4px',
               borderColor: staking
                 ? theme.palette.custom.forbole.indigo
                 : theme.palette.primary.main,
-              [theme.breakpoints.down("laptop")]: {
-                display: "none",
+              [theme.breakpoints.down('laptop')]: {
+                display: 'none',
               },
             }}
           />
-          {/* <Link href="/"> */}
-          <Typography
-            css={styles.word}
-            sx={{
-              opacity: "0.3",
-            }}
-          >
-            {t("blog")}
-          </Typography>
-          {/* </Link> */}
+          <Link href="/blog">
+            <Typography css={styles.word}>{t('blog')}</Typography>
+          </Link>
           {/* <Divider
             orientation="vertical"
             variant="middle"
