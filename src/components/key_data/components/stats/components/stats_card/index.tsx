a6f8d00ef06d817e53fa0fbac6077975c11fbd6a
Ignacio <ignacio@forbole.com>
2023-11-14 22:03:43 +0800

    chore: update component

--- src/components/key_data/components/stats/components/stats_card/index.tsx
+++ src/components/key_data/components/stats/components/stats_card/index.tsx
@@ -1,31 +1,33 @@
 import { Box, Typography } from "@mui/material";
 import { useCounter } from "@screens/staking/components/hooks";
 import { convertToMoney } from "@utils/convert_to_money";
+import useTranslation from "next-translate/useTranslation";
 
 import useStyles from "./useStyles";
 
 const StatsCard = ({ title, stats }: any) => {
   const styles = useStyles();
   const { counterValue, counterRef } = useCounter(stats);
+  const { t } = useTranslation("common");
 
   return (
     <Box ref={counterRef} css={styles.root}>
       <Typography variant="h6">{title}</Typography>
-      {title === "full tvl" && stats === "-" && (
+      {title === t("full tvl") && stats === "-" && (
         <Typography variant="h3">{`$ ${stats}`}</Typography>
       )}
-      {title === "full tvl" && stats !== "-" && (
+      {title === t("full tvl") && stats !== "-" && (
         <Typography variant="h3">{`$ ${convertToMoney(
           counterValue,
         )}`}</Typography>
       )}
-      {title === "users staking" && stats === "-" && (
+      {title === t("users staking") && stats === "-" && (
         <Typography variant="h3">{`${stats}`}</Typography>
       )}
-      {title === "users staking" && stats !== "-" && (
+      {title === t("users staking") && stats !== "-" && (
         <Typography variant="h3">{convertToMoney(counterValue)}</Typography>
       )}
-      {title === "supporting networks" && (
+      {title === t("supporting networks") && (
         <Typography variant="h3">{convertToMoney(counterValue)}</Typography>
       )}
     </Box>
