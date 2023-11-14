dda2eb9002bb465d526409aaab8245eec510dc49
Magic Cat <37407870+MonikaCat@users.noreply.github.com>
2023-11-14 18:17:33 +0700

    fix: remove forbole academy (#99)
    Fixes [FBW-123](https://forbole.atlassian.net/browse/FBW-123)
    
    [FBW-123]:
    https://forbole.atlassian.net/browse/FBW-123?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -28,6 +28,18 @@ const Home: FC<Props> = ({ pages }) => {
   const bottomRef = useRef(null);
   const router = useRouter();
   const { isMobile, isTablet } = useWindowDimensions();
+  const filteredPages = pages.reduce((pageList: Page[], current) => {
+    // filter out forbole academy page
+    if (
+      current.btnClick !==
+      "https://www.eventbrite.hk/e/-aug-22-29-tickets-686420260477?ref=forbole"
+    ) {
+      pageList.push(current);
+    }
+
+    return pageList;
+  }, []);
+
   return (
     <Layout
       description={t("description")}
@@ -228,8 +240,14 @@ const Home: FC<Props> = ({ pages }) => {
           >
             {t("What_is_New?")}
           </Typography>
-          <Grid container spacing={theme.spacing(2)}>
-            {pages.map(
+          <Grid
+            container
+            spacing={theme.spacing(2)}
+            sx={{
+              justifyContent: "center",
+            }}
+          >
+            {filteredPages.map(
               ({ title, list, imageHref, btnName, btnClick, id }, idx) => (
                 <Grid key={`${id}_${idx}`} item laptop={4} mobile={12}>
                   <IntroCard
