5e3ed535783dbd0e35549f4e126254d38e872df9
Ignacio <ignacio@forbole.com>
2023-12-11 15:29:38 +0800

    refactor: nav menus style

--- src/components/nav/components/company_menu_button/index.tsx
+++ src/components/nav/components/company_menu_button/index.tsx
@@ -1,21 +1,11 @@
-import { useMediaQuery, useTheme } from "@mui/material";
-import List from "@mui/material/List";
-import MenuItem from "@mui/material/MenuItem";
 import useTranslation from "next-translate/useTranslation";
-import Link from "next/link";
-import { useContext, useMemo } from "react";
+import { useMemo } from "react";
 
-import { anchorElContext } from "@src/utils/menu";
+import MenuList from "../menu_list";
 
 const CompanyMenuButton = () => {
   const { t } = useTranslation("common");
 
-  const theme = useTheme();
-  const onlyLargeScreen = useMediaQuery(theme.breakpoints.up("laptop"));
-  const { setAnchorEl } = useContext(anchorElContext);
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
   const menuCompanyList = useMemo(
     () => [
       {
@@ -34,79 +24,7 @@ const CompanyMenuButton = () => {
     [t],
   );
 
-  return onlyLargeScreen ? (
-    <List component="div" disablePadding>
-      {menuCompanyList?.map((l, i) => (
-        <div key={i}>
-          <Link href={l.link} style={{ textDecoration: "none" }}>
-            <MenuItem
-              component="span"
-              sx={{
-                "display": "flex",
-                "filter": l.link ? "" : "opacity(0.1)",
-                "width": "220px",
-                "color": theme.palette.custom.forbole.indigo,
-                "padding": "0px 32px",
-                "justifyContent": "flex-start",
-                "fontSize": theme.spacing(2),
-                "fontWeight": 700,
-                "height": theme.spacing(7),
-                "> a": {
-                  width: "100%",
-                  textAlign: "left",
-                  textDecoration: "none",
-                },
-                "&:hover": {
-                  color: theme.palette.common.white,
-                  background: "linear-gradient(to right,#623DF5,#362187)",
-                },
-              }}
-            >
-              {l.name}
-            </MenuItem>
-          </Link>
-        </div>
-      ))}
-    </List>
-  ) : (
-    <List
-      component="div"
-      disablePadding
-      sx={{ background: "rgba(107, 97, 254, 0.24)" }}
-    >
-      {menuCompanyList?.map((l, i) => (
-        <div key={i}>
-          <Link href={l.link} style={{ textDecoration: "none" }}>
-            <MenuItem
-              component="span"
-              onClick={handleClose}
-              sx={{
-                "display": "flex",
-                "justifyContent": "flex-start",
-                "fontSize": theme.spacing(2),
-                "fontWeight": 700,
-                "height": theme.spacing(7),
-                "padding": theme.spacing(0, 4),
-                "color": theme.palette.custom.forbole.indigo,
-                "filter": l.link ? "" : "opacity(0.1)",
-                "> a": {
-                  width: "100%",
-                  textAlign: "left",
-                  textDecoration: "none",
-                },
-                "&:hover": {
-                  color: theme.palette.common.white,
-                  background: "linear-gradient(to right,#623DF5,#362187)",
-                },
-              }}
-            >
-              {l.name}
-            </MenuItem>
-          </Link>
-        </div>
-      ))}
-    </List>
-  );
+  return <MenuList menuList={menuCompanyList} />;
 };
 
 export default CompanyMenuButton;
--- src/components/nav/components/products_menu_button/index.tsx
+++ src/components/nav/components/products_menu_button/index.tsx
@@ -1,22 +1,11 @@
-import { useMediaQuery, useTheme } from "@mui/material";
-import List from "@mui/material/List";
-import MenuItem from "@mui/material/MenuItem";
 import useTranslation from "next-translate/useTranslation";
-import Link from "next/link";
-import { useContext, useMemo } from "react";
+import { useMemo } from "react";
 
-import { anchorElContext } from "@src/utils/menu";
+import MenuList from "../menu_list";
 
 const ProductsMenuButton = () => {
   const { t } = useTranslation("common");
 
-  const theme = useTheme();
-  const onlyLargeScreen = useMediaQuery(theme.breakpoints.up("laptop"));
-
-  const { setAnchorEl } = useContext(anchorElContext);
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
   const menuProductsList = useMemo(
     () => [
       {
@@ -43,76 +32,7 @@ const ProductsMenuButton = () => {
     [t],
   );
 
-  return onlyLargeScreen ? (
-    <List component="div" disablePadding>
-      {menuProductsList?.map((l, i) => (
-        <div key={i}>
-          <Link href={l.link} style={{ textDecoration: "none" }}>
-            <MenuItem
-              component="span"
-              sx={{
-                "display": "flex",
-                "padding": "0px 32px",
-                "justifyContent": "flex-start",
-                "fontSize": theme.spacing(2),
-                "fontWeight": 700,
-                "height": theme.spacing(7),
-                "filter": l.link !== "#!" ? "" : "opacity(0.1)",
-                "color": theme.palette.custom.forbole.indigo,
-                "> a": {
-                  width: "100%",
-                  textAlign: "left",
-                  textDecoration: "none",
-                },
-                "&:hover": {
-                  color: theme.palette.common.white,
-                  background: "linear-gradient(to right,#623DF5,#362187)",
-                },
-              }}
-            >
-              {l.name}
-            </MenuItem>
-          </Link>
-        </div>
-      ))}
-    </List>
-  ) : (
-    <List
-      component="div"
-      disablePadding
-      sx={{ background: "rgba(107, 97, 254, 0.24)" }}
-    >
-      {menuProductsList?.map((l, i) => (
-        <Link href={l.link} key={i} style={{ textDecoration: "none" }}>
-          <MenuItem
-            component="span"
-            onClick={handleClose}
-            sx={{
-              "display": "flex",
-              "justifyContent": "flex-start",
-              "fontSize": theme.spacing(2),
-              "fontWeight": 700,
-              "height": theme.spacing(7),
-              "padding": theme.spacing(0, 4),
-              "filter": l.link !== "#!" ? "" : "opacity(0.1)",
-              "color": theme.palette.custom.forbole.indigo,
-              "> a": {
-                width: "100%",
-                textAlign: "left",
-                textDecoration: "none",
-              },
-              "&:hover": {
-                color: theme.palette.common.white,
-                background: "linear-gradient(to right,#623DF5,#362187)",
-              },
-            }}
-          >
-            {l.name}
-          </MenuItem>
-        </Link>
-      ))}
-    </List>
-  );
+  return <MenuList menuList={menuProductsList} />;
 };
 
 export default ProductsMenuButton;
