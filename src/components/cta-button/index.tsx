2bfccfa607f45114ec981a9cbc8ef7d2f3de5863
Ignacio <ignacio@forbole.com>
2024-01-04 12:14:07 +0800

    chore: replace material button with vanilla one

--- src/components/cta-button/index.tsx
+++ src/components/cta-button/index.tsx
@@ -1,33 +1,21 @@
-import type { LoadingButtonProps } from "@mui/lab";
-import { LoadingButton } from "@mui/lab";
-import { styled } from "@mui/material/styles";
+import type { ButtonHTMLAttributes } from "react";
 
-const ColorButton = styled(LoadingButton)<LoadingButtonProps>(() => ({
-  "&:disabled": {
-    background: "#EBEEF5",
-    boxShadow:
-      "0px 10px 32px -4px rgba(125, 92, 255, 0.1),0px 6px 14px -6px rgba(126, 94, 255, 0.28)",
-    color: "rgba(195, 204, 226, 1)",
-  },
-  "&:hover": {
-    background:
-      "linear-gradient(175.41deg, #623DF5 11.57%, #362187 173.5%),linear-gradient(0deg, rgba(0, 0, 0, 0.32), rgba(0, 0, 0, 0.32))",
-    boxShadow:
-      "0px 10px 32px -4px rgba(125, 92, 255, 0.1), 0px 6px 14px -6px rgba(126, 94, 255, 0.28)",
-    transform: "scale(1.02)",
-  },
-  "background": "linear-gradient(175.41deg, #623DF5 11.57%, #362187 173.5%)",
-  "borderRadius": "45px",
-  "color": "rgba(235, 238, 245, 1)",
-  "flexShrink": 0,
-  "fontSize": "16px",
-  "fontWeight": "590",
-  "height": "45px",
-  "padding": " 0px 16px",
-  "transition": "transform 0.2s ease-in-out",
-  "whiteSpace": "nowrap",
-}));
+import * as styles from "./index.module.scss";
 
-export default function CtaButton(props: LoadingButtonProps) {
-  return <ColorButton {...props} />;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  loading?: boolean;
+};
+
+export default function CtaButton({
+  disabled,
+  loading,
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      {...props}
+      className={[styles.button, props.className].join(" ")}
+      disabled={disabled || loading}
+    />
+  );
 }
