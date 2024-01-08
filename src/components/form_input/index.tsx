74f5dce99bf8f9b3e1d41c7bbf94681a15a5a690
Ignacio <ignacio@forbole.com>
2024-01-08 21:38:03 +0800

    chore: refactor input

--- src/components/form_input/index.tsx
+++ src/components/form_input/index.tsx
@@ -1,47 +1,9 @@
-import InputBase from "@mui/material/InputBase";
-import { styled } from "@mui/material/styles";
+import * as styles from "./index.module.scss";
 
-const FormInput = styled(InputBase)(({ theme }) => ({
-  "& .MuiInputBase-input": {
-    "&:focus": {
-      borderColor: theme.palette.custom.forbole.indigo,
-    },
-    "backgroundColor": theme.palette.mode === "light" ? "#FFF" : "#1A2027",
-    "border": "2px solid",
-    "borderColor": theme.palette.mode === "light" ? "#fff" : "#2D3843",
-    "borderRadius": "8px",
-    "display": "block",
-    // Use the system font instead of the default Roboto font.
-    "fontFamily": [
-      "-apple-system",
-      "BlinkMacSystemFont",
-      '"Segoe UI"',
-      "Roboto",
-      '"Helvetica Neue"',
-      "Arial",
-      "sans-serif",
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(","),
-    "fontSize": "16px",
-    "marginTop": theme.spacing(2),
-    "padding": "12px 16px",
-    "position": "relative",
-    "transition": theme.transitions.create([
-      "border-color",
-      "background-color",
-      "box-shadow",
-    ]),
-    "width": "calc( 100% - 36px)",
-  },
-  "& .MuiInputBase-input:placeholder": {
-    color: "#878787",
-  },
-  "borderRadius": "8px",
+type Props = React.InputHTMLAttributes<HTMLInputElement>;
 
-  "boxShadow": "4px 8px 24px 0px rgba(116, 81, 255, 0.28)",
-  "display": "block",
-}));
+const FormInput = ({ className, ...props }: Props) => (
+  <input {...props} className={[styles.input, className].join(" ")} />
+);
 
 export default FormInput;
